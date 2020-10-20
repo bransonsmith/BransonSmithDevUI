5bf51645aa68a848ac5aea9f47dae7fd207b263a
Branson Smith <bransonsmith22@gmail.com>
2020-10-19 21:17:49 -0500

    Add a placeholder home page

--- branson-smith-dev/src/app/app.module.ts
+++ branson-smith-dev/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
